522e788ef0b71a024a24eb560174e42ee8e2acba
gabedarley <69703855+gabedarley@users.noreply.github.com>
2021-03-31 10:06:53 -0500

    Update Bird.py

--- Bird.py
+++ Bird.py
@@ -1,4 +1,6 @@
 from Clearing import Clearing
+#from Card import Card
+
 class Bird:
 
     def __init__(self):
@@ -7,3 +9,16 @@ class Bird:
 
         #List to store locations of roosts, type Clearing
         self.roostLoc = []
+
+        #Leader, type String
+        self.leader = ""
+
+        #type Card
+        self.handCards = []
+        self.craftedCards = []
+
+        #Suit only, type String
+        self.recruitCards = []
+        self.moveCards = []
+        self.battleCards = []
+        self.buildCards = []
