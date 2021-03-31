7de2be931bd8040a7c2eeab89e6dadaacfc4f218
gabedarley <69703855+gabedarley@users.noreply.github.com>
2021-03-31 09:57:45 -0500

    Add files via upload

---
+++ Bird.py
@@ -0,0 +1,9 @@
+from Clearing import Clearing
+class Bird:
+
+    def __init__(self):
+        self.warriorReserve = 20
+        self.roostReserve = 7
+
+        #List to store locations of roosts, type Clearing
+        self.roostLoc = []
---
+++ Cat.py
@@ -0,0 +1,14 @@
+from Clearing import Clearing
+class Cat:
+
+    def __init__(self):
+        self.warriorReserve = 25
+        self.woodReserve = 8
+        self.sawmillReserve = 6
+        self.workshopReserve = 6
+        self.recruiterReserve = 6
+
+        #Lists to store locations of buildings, type Clearing
+        self.sawmillLoc = []
+        self.workshoplLoc = []
+        self.recruiterLoc = []
