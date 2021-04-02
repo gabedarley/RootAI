b10b18b739a45ff2b7599522b4ae2a49fe22e921
gabedarley <69703855+gabedarley@users.noreply.github.com>
2021-04-02 12:44:42 -0500

    Update Clearing.py

--- Clearing.py
+++ Clearing.py
@@ -1,4 +1,3 @@
-import random
 class Clearing:
 
     def __init__(self, id, slots, suit):
@@ -11,68 +10,88 @@ class Clearing:
         self.slots = slots
         self.suit = suit
 
-    def AddAdjacent(self,adjacentClear):
+    def AddAdjacent(self, adjacentClear):
         #adjacentClear must be clearing object
         self.adjacent.append(adjacentClear)
 
     def GetAdjacent(self):
         return self.adjancent
 
-    def AddWarrior(self,warriorType):
-        if warriorType == "bird":
+    def AddWarrior(self, player):
+        #player is type Bird or Cat
+        if player.id == "bird":
             self.birdWarriors += 1
-            #TODO: Update bird's warrior reserve
-        if warriorType == "cat":
+            player.warriorReserve -= 1
+        if player.id == "cat":
             self.catWarriors += 1
-            #TODO: Update cat's warrior reserve
+            player.warriorReserve -= 1
 
-    def RemoveWarrior(self,warriorType):
-        if warriorType == "bird":
+    def RemoveWarrior(self, player):
+        #player is type Bird or Cat
+        if player.id == "bird":
             self.birdWarriors -= 1
-            #TODO: Update bird's warrior reserve
-        if warriorType == "cat":
+            player.warriorReserve += 1
+        if player.id == "cat":
             self.catWarriors -= 1
-            #TODO: Update cat's warrior reserve
+            player.warriorReserve += 1
 
-    def AddBuilding(self,buildingType):
+    def AddBuilding(self, buildingType, player):
         #buildingType must be roost, sawmill, workshop, or recruiter
         self.Buildings.append(buildingType)
         self.slots -= 1
-        #TODO: Update Bird's roost reserve
-        #TODO: Update Cat's score
-        #TODO: Update Cat's building reserve
-    def AddToken(self, tokenType):
+
+        if player.id == "bird":
+            player.roostReserve -= 1
+        elif player.id == "cat":
+            if buildingType == "sawmill":
+                player.sawmillReserve -= 1
+            elif buildType == "workshop":
+                player.workshopReserve -= 1
+            else:
+                player.recruiterReserve -= 1
+
+    def AddToken(self, tokenType, player):
         if tokenType == "keep":
             self.catTokens.append("keep")
         elif tokenType == "wood":
             self.catTokens.append("wood")
-            #TODO: Update Cat's token reserve
-    def RemoveWood(self):
-        self.catTokens.remove("wood")
-        #TODO: Update Cat's token reserve
+            player.woodReserve -= 1
 
-    def RemoveBuilding(self,buildingType):
+    def RemoveWood(self, player):
+        self.catTokens.remove("wood")
+        player.woodReserve += 1
+
+    def RemoveBuilding(self, buildingType):
         #buildingType must be roost, sawmill, workshop, or recruiter
         self.Buildings.remove(buildingType)
         self.slots += 1
-        #TODO: Update Bird's roost reserve
-        #TODO: Update Cat's building reserve
+        if player.id == "bird":
+            player.roostReserve += 1
+        elif player.id == "cat":
+            if buildingType == "sawmill":
+                player.sawmillReserve += 1
+            elif buildType == "workshop":
+                player.workshopReserve += 1
+            else:
+                player.recruiterReserve += 1
 
-    def GetNumWarrior(self,warriorType):
+    def GetNumWarrior(self, warriorType):
         if warriorType == "bird":
             return self.birdWarriors
         if warriorType == "cat":
             return self.catWarriors
+
     def HasKeep(self):
         if "keep" in self.catTokens:
             return True
+
     def GetNumWood(self):
         temp = self.catTokens
         if "keep" in temp:
             temp.remove("keep")
         return len(temp)
 
-    def Battle(self,attacker):
+    def Battle(self, attacker, defender):
         #TODO: Prompt for ambush card ?
         #attacker must be a string
         rolls = [random.randint(0,3), random.randint(0,3)]
@@ -82,7 +101,7 @@ class Clearing:
         print("Roll 2: %d\n" % roll2)
 
         '''CAT ATTACKS'''
-        if attacker == "cat":
+        if attacker.id == "cat":
             #Find max attack power for attacker and defender
             attackMax = self.catWarriors
             defendMax = self.birdWarriors
@@ -96,18 +115,18 @@ class Clearing:
             #Check which bird pieces to remove
             if self.birdWarriors >= roll1:
                 self.birdWarriors -= roll1
-                #TODO: Update Bird's warrior reserve
+                defender.warriorReserve += roll1
             elif self.birdWarriors == 0:
                 if "roost" in self.Buildings:
                     self.Buildings.remove("roost")
-                    #TODO: Update Bird's building reserve
+                    defender.roostReserve += 1
             else:
                 warriorDiff = roll1 - self.birdWarriors
                 self.birdWarriors = 0
                 if "roost" in self.Buildings:
                     self.Buildings.remove("roost")
-                    #TODO: Update Bird's building reserve
-                    #TODO: Update Bird's warrior reserve
+                    defender.roostReserve += 1
+                    defender.warriorReserve += warriorDiff
 
             #Check which cat pieces to remove
             if self.catWarriors >= roll2:
@@ -118,12 +137,16 @@ class Clearing:
                 while numRemoved != roll2:
                     if "wood" in self.catTokens:
                         self.catTokens.remove("wood")
+                        attacker.woodReserve += 1
                     elif "workshop" in self.Buildings:
                         self.Buildings.remove("workshop")
+                        attacker.workshopReserve += 1
                     elif "recruiter" in self.Buildings:
                         self.Buildings.remove("recruiter")
+                        attacker.recruiterReserve += 1
                     elif "sawmill" in self.Buildings:
                         self.Buildings.remove("sawmill")
+                        attacker.sawmillReserve += 1
                     elif "keep" in self.catTokens:
                         self.catTokens.remove("keep")
                     numRemoved += 1
@@ -133,19 +156,23 @@ class Clearing:
                 while numRemoved != warriorDiff:
                     if "wood" in self.catTokens:
                         self.catTokens.remove("wood")
+                        attacker.woodReserve += 1
                     elif "workshop" in self.Buildings:
                         self.Buildings.remove("workshop")
+                        attacker.workshopReserve += 1
                     elif "recruiter" in self.Buildings:
                         self.Buildings.remove("recruiter")
+                        attacker.recruiterReserve += 1
                     elif "sawmill" in self.Buildings:
                         self.Buildings.remove("sawmill")
+                        attacker.sawmillReserve += 1
                     elif "keep" in self.catTokens:
                         self.catTokens.remove("keep")
                     numRemoved += 1
 
 
         '''BIRD ATTACKS'''
-        if attacker == "bird":
+        if attacker.id == "bird":
             #Find max attack power for attacker and defender
             attackMax = self.birdWarriors
             defendMax = self.catWarriors
@@ -159,48 +186,62 @@ class Clearing:
             #Check which bird pieces to remove
             if self.birdWarriors >= roll2:
                 self.birdWarriors -= roll2
-                #TODO: Update Bird's warrior reserve
+                attacker.warriorReserve += roll2
             elif self.birdWarriors == 0:
                 if "roost" in self.Buildings:
                     self.Buildings.remove("roost")
-                #TODO: Update Bird's building reserve
+                    attacker.roostReserve += 1
             else:
+                warriorDiff = roll1 - self.birdWarriors
                 self.birdWarriors = 0
                 if "roost" in self.Buildings:
                     self.Buildings.remove("roost")
-                    #TODO: Update Bird's building reserve
-                    #TODO: Update Bird's warrior reserve
+                    attacker.roostReserve += 1
+                    attacker.warriorReserve += warriorDiff
 
             #Check which cat pieces to remove
             if self.catWarriors >= roll1:
                 self.catWarriors -= roll1
-                #TODO Update Cat's warrior reserve
+                defender.warriorReserve += roll1
             elif self.catWarriors == 0:
                 numRemoved = 0
                 while numRemoved != roll1:
                     if "wood" in self.catTokens:
                         self.catTokens.remove("wood")
+                        defender.woodReserve += 1
                     elif "workshop" in self.Buildings:
                         self.Buildings.remove("workshop")
+                        defender.workshopReserve += 1
                     elif "recruiter" in self.Buildings:
                         self.Buildings.remove("recruiter")
+                        defender.recruiterReserve += 1
                     elif "sawmill" in self.Buildings:
                         self.Buildings.remove("sawmill")
+                        defender.sawmillReserve += 1
                     elif "keep" in self.catTokens:
                         self.catTokens.remove("keep")
                     numRemoved += 1
             else:
                 warriorDiff = roll1 - self.catWarriors
                 self.catWarriors = 0
+                defender.warriorReserve += warriorDiff
                 while numRemoved != warriorDiff:
                     if "wood" in self.catTokens:
                         self.catTokens.remove("wood")
+                        defender.woodReserve += 1
                     elif "workshop" in self.Buildings:
                         self.Buildings.remove("workshop")
+                        defender.workshopReserve += 1
                     elif "recruiter" in self.Buildings:
                         self.Buildings.remove("recruiter")
+                        defender.recruiterReserve += 1
                     elif "sawmill" in self.Buildings:
                         self.Buildings.remove("sawmill")
+                        defender.sawmillReserve += 1
                     elif "keep" in self.catTokens:
                         self.catTokens.remove("keep")
                     numRemoved += 1
+
+import random
+from Cat import Cat
+from Bird import Bird
