f525cae2655cb0bd05bed5114f1dde32417d7239
Eddie-Moy <69971590+Eddie-Moy@users.noreply.github.com>
2021-04-13 12:54:41 -0500

    Update Bird.py

--- Bird.py
+++ Bird.py
@@ -6,7 +6,7 @@ class Bird:
 
         #List to store locations of roosts, type Clearing
         self.roostLoc = []
-        self.crafting = {"fox": 0, "bunny": 0, "mouse:" 0}
+        self.crafting = {"fox": 0, "bunny": 0, "mouse": 0}
 
         self.id = "bird"
 
@@ -14,14 +14,18 @@ class Bird:
         self.leader = ""
 
         #type Card
-        self.handCards = []
-        self.craftedCards = []
+        self.handCards = {}
 
         #Suit only, type String
         self.recruitCards = []
         self.moveCards = []
         self.battleCards = []
         self.buildCards = []
-
+    def DrawCard(self,board):
+        num = random.randint(0,54)
+        #handCards.append(board.deck[num])
+        self.handCards[board.deck[0].id] = board.deck[0]
+import random
 from Clearing import Clearing
 from Card import Card
+from Board import Board
